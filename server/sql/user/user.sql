efc382e316649bd19918357650c1ac3a90eeb46a
Maksym Shapovalov <maks0681912507@gmail.com>
2023-04-03 14:44:07 +0300

    Feat(sql): add users schemas

---
+++ server/sql/user/user.sql
@@ -0,0 +1,18 @@
+USE `unsistudproj`;
+
+DROP TABLE IF EXISTS `tbl_users`;
+CREATE TABLE `tbl_users`
+(
+    `user_id`  INT,
+    `role_id`  INT          NOT NULL,
+    `name`     VARCHAR(30)  NOT NULL,
+    `email`    VARCHAR(30)  NOT NULL,
+    `password` VARCHAR(30)  NOT NULL,
+    `username` VARCHAR(30)  NOT NULL,
+    `about`    VARCHAR(200) NULL,
+    `group`    VARCHAR(30)  NOT NULL,
+    CONSTRAINT `users_PK_user_id` PRIMARY KEY (`user_id`),
+    CONSTRAINT `users_FK_user_roles_role_id` FOREIGN KEY (`role_id`) REFERENCES `tbl_user_roles` (`role_id`) ON DELETE RESTRICT,
+    CONSTRAINT `users_UQ_email` UNIQUE (`email`),
+    CONSTRAINT `users_UQ_username` UNIQUE (`username`)
+);
