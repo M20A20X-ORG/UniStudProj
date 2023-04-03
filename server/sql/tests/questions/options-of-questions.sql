1369162b0b7e216f45cb5aec1ef78033c2f977fe
Maksym Shapovalov <maks0681912507@gmail.com>
2023-04-03 16:04:19 +0300

    Feat(sql): add tests schemas

---
+++ server/sql/tests/questions/options-of-questions.sql
@@ -0,0 +1,11 @@
+USE `unsistudproj`;
+
+DROP TABLE IF EXISTS `tbl_options_of_questions`;
+CREATE TABLE `tbl_options_of_questions`
+(
+    `question_id` INT,
+    `option_id`   INT,
+    CONSTRAINT `options_of_questions_PK_question_id_option_id` PRIMARY KEY (`question_id`, `option_id`),
+    CONSTRAINT `options_of_questions_FK_questions_question_id` FOREIGN KEY (`question_id`) REFERENCES `tbl_questions` (`question_id`) ON DELETE CASCADE,
+    CONSTRAINT `options_of_questions_FK_question_options_option_id` FOREIGN KEY (`option_id`) REFERENCES `tbl_question_options` (`option_id`) ON DELETE RESTRICT
+);
---
+++ server/sql/tests/questions/question-type.sql
@@ -0,0 +1,10 @@
+USE `unsistudproj`;
+
+DROP TABLE IF EXISTS `tbl_question_types`;
+CREATE TABLE `tbl_question_types`
+(
+    `type_id` INT,
+    `name`    VARCHAR(30) NOT NULL,
+    CONSTRAINT `question_types_PK_type_id` PRIMARY KEY (`type_id`),
+    CONSTRAINT `question_types_UQ_name` UNIQUE (`name`)
+);
