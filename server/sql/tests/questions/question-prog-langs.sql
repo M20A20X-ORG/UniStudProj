896ae75de849c9f2e4fee7e539e10d3b01dd3a1b
Maksym Shapovalov <maks0681912507@gmail.com>
2023-04-03 16:04:19 +0300

    Feat(sql): add tests schemas

---
+++ server/sql/tests/questions/question-prog-langs.sql
@@ -0,0 +1,10 @@
+USE `unsistudproj`;
+
+DROP TABLE IF EXISTS `tbl_question_prog_langs`;
+CREATE TABLE `tbl_question_prog_langs`
+(
+    `prog_lang_id` INT,
+    `name`         VARCHAR(30) NOT NULL,
+    CONSTRAINT `question_prog_langs_PK_prog_lang_id` PRIMARY KEY (`prog_lang_id`),
+    CONSTRAINT `question_prog_langs_UQ_name` UNIQUE (`name`)
+);
