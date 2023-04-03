da50efa5b7956b6801d628faf30217149d948ab0
Maksym Shapovalov <maks0681912507@gmail.com>
2023-04-03 14:43:12 +0300

    Feat(sql): configure database

---
+++ server/sql/config.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE `unsistudproj`;
+USE `unsistudproj`;
+ALTER DATABASE `unsistudproj` CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci;
+SELECT @@character_set_database, @@collation_database;
