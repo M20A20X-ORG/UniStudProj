0efcc9f7c8d920535319e7825ae76f03afb76043
Maksym Shapovalov <maks0681912507@gmail.com>
2023-04-05 13:54:54 +0300

    Fix(serv-common): fix env config, logger types, code style

--- server/src/routes/tests/questions.ts
+++ server/src/routes/tests/questions.ts
@@ -5,4 +5,5 @@ const router = createChildRouter();
 router.post('create/', (req, res) => res.json('create'));
 router.put('/edit', (req, res) => res.json('edit'));
 router.post('delete/', (req, res) => res.json('delete'));
+
 export const questionsRouter = router;
