02d8833114ca6cbf881f8529c910224782afeff9
Maksym Shapovalov <maks0681912507@gmail.com>
2023-04-15 02:10:10 +0300

    Feat(serv-common): add custom schema formats

--- server/src/configs/schemas.ts
+++ server/src/configs/schemas.ts
@@ -1,3 +1,13 @@
 import Ajv from 'ajv';
+import { TSchemaPatterns } from '@type/schema';
 
 export const ajv = new Ajv({ allErrors: true });
+
+const SCHEMA_PATTERNS: TSchemaPatterns = {
+    fullname: /^([A-Z][a-z]+\s?){3,}$/i,
+    username: /^[a-z\d]{5,}$/,
+    password: /^[\w\W]{6,}$/,
+    email: /^[a-z\d]+@[a-z\d]+.[a-z\d]+$/
+};
+
+Object.keys(SCHEMA_PATTERNS).forEach((key) => ajv.addFormat(key, SCHEMA_PATTERNS[key]));
---
+++ server/src/types/schema.ts
@@ -0,0 +1,1 @@
+export type TSchemaPatterns = { [key: string]: RegExp };
