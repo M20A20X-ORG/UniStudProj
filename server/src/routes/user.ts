5d8fc8e48822e012b3edcdc3ec5c1b0795f57139
Maksym Shapovalov <maks0681912507@gmail.com>
2023-04-15 06:24:00 +0300

    Feat(user-service): add RD (CRUD) operations

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,10 +1,22 @@
 import { createChildRouter } from '@utils/router';
+import { ACCESS_ROLE } from '@configs/auth';
+
+import { requireAuth } from '@middleware/auth';
+import { requireQueryValidator } from '@middleware/validateQuery';
+
+import { usersController } from '@controllers/users';
 
 const router = createChildRouter();
+const { admin, user } = ACCESS_ROLE;
 
 router.post('/register', (req, res) => res.json('register'));
-router.get('/get', (req, res) => res.json('get'));
-router.put('/edit', (req, res) => res.json('edit'));
-router.post('/delete', (req, res) => res.json('delete'));
+router.get('/get', requireQueryValidator('user'), usersController.getGetUser);
+router.put('/edit', requireAuth(user), (req, res) => res.json('get'));
+router.delete(
+    '/delete',
+    requireAuth(admin, user),
+    requireQueryValidator('userId'),
+    usersController.deleteDeleteUser
+);
 
 export const usersRouter = router;
--- server/src/services/users.ts
+++ server/src/services/users.ts
@@ -1,12 +1,71 @@
+import { TPayloadResponse, TResponse } from '@type/schemas/response';
 import { TUser, TUserPartial } from '@type/schemas/user';
 
+import { DataDeletionError } from '@exceptions/DataDeletionError';
+import { NoDataError } from '@exceptions/NoDataError';
+
+import { sqlPool } from '@configs/sqlPool';
+
 interface UsersService {
-    getUser?: (userIdentifier: number | string) => TUser;
-    registerUser?: (userData: TUser) => string;
-    editUser?: (userData: TUserPartial) => string;
-    deleteUser?: (userId: number) => string;
+    getUser: (userIdentifier: number | string) => Promise<TPayloadResponse<TUser>>;
+    registerUser?: (userData: TUser) => Promise<TResponse>;
+    editUser?: (userData: TUserPartial) => Promise<TPayloadResponse<TUser>>;
+    deleteUser: (userId: number) => Promise<TResponse>;
 }
 
-class UsersServiceImpl implements UsersService {}
+class UsersServiceImpl implements UsersService {
+    public deleteUser = async (userId: number): Promise<TResponse> => {
+        const sql = {
+            selectUserEmail: `SELECT email
+                        FROM tbl_users
+                        WHERE user_id = ?`,
+            deleteUser: `DELETE
+                   FROM tbl_users
+                   WHERE user_id = ?`
+        };
+        const dbUserEmailResponse = await sqlPool.query(sql.selectUserEmail, userId);
+        const [[dbUserEmail]] = dbUserEmailResponse;
+        if (!dbUserEmail) throw new DataDeletionError(`No user found, id: '${userId}'`);
+
+        const { email } = dbUserEmail;
+        await sqlPool.query(sql.deleteUser, userId);
+
+        return new Promise<TResponse>((resolve) =>
+            resolve({
+                message: `Successfully deleted user, email: '${email}'`
+            })
+        );
+    };
+
+    public getUser = async (userIdentifier: number | string): Promise<TPayloadResponse<TUser>> => {
+        const sql = `SELECT u.user_id AS userId,
+                        ur.name   AS role,
+                        u.*
+                 FROM (SELECT user_id,
+                              role_id,
+                              name,
+                              email,
+                              password,
+                              username,
+                              about,
+                              'group'
+                       FROM tbl_users
+                       WHERE ${
+                           typeof userIdentifier === 'string' ? 'username' : 'user_id'
+                       } = ?) AS u
+                          JOIN tbl_user_roles ur ON ur.role_id = u.role_id`;
+
+        const dbUserResponse = await sqlPool.query(sql, userIdentifier);
+        const [[dbUser]] = dbUserResponse;
+        if (!dbUser) throw new NoDataError(`No user found, identifier: '${userIdentifier}'`);
+
+        return new Promise<TPayloadResponse<TUser>>((resolve) =>
+            resolve({
+                message: `Successfully got user, identifier: '${userIdentifier}'`,
+                payload: dbUser
+            })
+        );
+    };
+}
 
 export const usersService = new UsersServiceImpl();
