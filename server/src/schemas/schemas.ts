8bf6f740fc5658295615f8e86f56a85ab35dab95
Maksym Shapovalov <maks0681912507@gmail.com>
2023-04-15 03:41:00 +0300

    Feat(user-service): add ajv schemas

---
+++ server/src/schemas/schemas.ts
@@ -0,0 +1,5 @@
+import { userSchemas } from '@schemas/users';
+
+export const schemas = {
+    userSchemas
+};
---
+++ server/src/schemas/users/index.ts
@@ -0,0 +1,9 @@
+import { loginSchema } from '@schemas/users/loginSchema';
+import { registrationSchema } from '@schemas/users/registrationSchema';
+import { editSchema } from '@schemas/users/editSchema';
+
+export const userSchemas = {
+    loginSchema,
+    registrationSchema,
+    editSchema
+};
