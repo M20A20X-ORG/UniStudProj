f6eacae5bcf9fda492b8c336648f3a6ceb79900f
Maksym Shapovalov <maks0681912507@gmail.com>
2023-04-26 18:50:58 +0300

    Fix(serv-dev): add/fix utils, types, move service sql

--- server/src/utils/concat.ts
+++ server/src/utils/concat.ts
@@ -1,4 +1,4 @@
-export const concat = (strings: string[], delim = ',', spaceStart = true) =>
+export const concat = (strings: any[], delim = ',', spaceStart = true) =>
     strings.reduce(
         (result, part) =>
             result.concat(part ? (result ? delim : spaceStart ? ' ' : '') + part : ''),
---
+++ server/src/utils/sendResponse.ts
@@ -0,0 +1,17 @@
+import { Response } from 'express';
+import { TResponse } from '@type/schemas/response';
+import { log } from '@configs/logger';
+
+export const sendResponse = (
+    res: Response,
+    status: number,
+    message: string,
+    stack: string | undefined
+): Response => {
+    if (status === 500) {
+        log.err(stack ?? message);
+        return res.sendStatus(status);
+    }
+    log.warn(message);
+    return res.status(status).json({ message } as TResponse);
+};
