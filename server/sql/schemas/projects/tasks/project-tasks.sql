a29cb60bc318892e0ecac417baebfcb93bb9a12b
Maksym Shapovalov <maks0681912507@gmail.com>
2023-04-26 19:16:39 +0300

    Fix(project-service): delete redundant schema

--- server/sql/schemas/projects/tasks/project-tasks.sql
+++ server/sql/schemas/projects/tasks/project-tasks.sql
@@ -4,11 +4,14 @@ DROP TABLE IF EXISTS `tbl_project_tasks`;
 CREATE TABLE `tbl_project_tasks`
 (
     `task_id`        INT AUTO_INCREMENT,
+    `project_id`     INT NOT NULL,
     `name`           VARCHAR(30)  NOT NULL,
     `description`    VARCHAR(200) NULL,
     `status_id`      INT          NULL,
     `assign_user_id` INT          NULL,
     CONSTRAINT `project_tasks_PK_task_id` PRIMARY KEY (`task_id`),
+    CONSTRAINT `project_tasks_UQ_task_name` UNIQUE (`name`),
+    CONSTRAINT `project_tasks_FK_projects_projects_id` FOREIGN KEY (`project_id`) REFERENCES `tbl_projects` (`project_id`) ON DELETE CASCADE,
     CONSTRAINT `project_tasks_FK_task_statuses_status_id` FOREIGN KEY (`status_id`) REFERENCES `tbl_task_statuses` (`status_id`) ON DELETE SET NULL,
     CONSTRAINT `project_tasks_FK_users_user_id` FOREIGN KEY (`assign_user_id`) REFERENCES `tbl_users` (`user_id`) ON DELETE SET NULL
 );
