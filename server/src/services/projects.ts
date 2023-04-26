b11bbe62ce25d1265a853b706b6a934cfd342175
Maksym Shapovalov <maks0681912507@gmail.com>
2023-04-26 18:50:58 +0300

    Fix(serv-dev): add/fix utils, types, move service sql

--- server/src/services/projects.ts
+++ server/src/services/projects.ts
@@ -1,10 +1,12 @@
 import { QueryError } from 'mysql2';
 
+import { TDependency, TGetSql } from '@type/sql';
 import { TPayloadResponse, TResponse } from '@type/schemas/response';
 import {
     TProject,
     TProjectCreation,
     TProjectEdit,
+    TProjectId,
     TProjectParticipant,
     TProjectParticipantId,
     TProjectTag
@@ -16,15 +18,9 @@ import { NoDataError } from '@exceptions/NoDataError';
 import { DataModificationError } from '@exceptions/DataModificationError';
 
 import { sqlPool } from '@configs/sqlPool';
-import { concat } from '@utils/concat';
+import { PROJECT_SQL } from '@static/sql/projects';
 
-type TDependency<T> = { projectId?: number } & T;
-type TGetSqlReturn = [undefined | string, undefined | string, string];
-type TGetSql<T> = (
-    projectId: number,
-    data: Array<T> | undefined,
-    deleteData: number[] | undefined
-) => TGetSqlReturn;
+const { readSql, deleteSql, createSql, updateSql } = PROJECT_SQL;
 
 interface ProjectsService {
     getProjects: (...projectIds: number[]) => Promise<TPayloadResponse<TProject[]>>;
@@ -34,129 +30,9 @@ interface ProjectsService {
 }
 
 class ProjectsServiceImpl implements ProjectsService {
-    private _sql = {
-        delete: {
-            selectProjectName: `SELECT name
-                          FROM tbl_projects
-                          WHERE project_id = ?`,
-            deleteProject: `DELETE
-                      FROM tbl_projects
-                      WHERE project_id = ?`,
-            deleteProjectUsers: `DELETE
-                           FROM tbl_users_of_projects
-                           WHERE project_id = ?`,
-            deleteProjectTags: `DELETE
-                          FROM tbl_tags_of_projects
-                          WHERE project_id = ?`
-        },
-        read: {
-            getSelectProjects: (projectIds: number[]) => `SELECT project_id          AS projectId,
-                                                           date_start          AS dateStart,
-                                                           date_end            AS dateEnd,
-                                                           participants_amount AS participantsAmount,
-                                                           tags_amount         AS tagsAmount,
-                                                           name,
-                                                           description
-                                                    FROM tbl_projects
-                                                    WHERE project_id IN (${projectIds.toString()})`,
-            getSelectTags: (
-                projectIds: number[]
-            ) => `SELECT tp.project_id AS projectId, pt.tag_id AS tagId, pt.name AS tag
-            FROM (SELECT project_id, tag_id
-                  FROM tbl_tags_of_projects
-                  WHERE project_id IN (${projectIds.toString()})) AS tp
-                     JOIN tbl_project_tags pt ON pt.tag_id = tp.tag_id`,
-            getSelectParticipants: (projectIds: number[]) => `SELECT up.project_id AS projectId,
-                                                               u.user_id     AS userId,
-                                                               u.username,
-                                                               pr.name       AS name,
-                                                               pr.role_id    AS roleId
-                                                        FROM (SELECT project_id, user_id, project_role_id
-                                                              FROM tbl_users_of_projects
-                                                              WHERE project_id IN (${projectIds.toString()})) AS up
-                                                                 JOIN tbl_users u ON u.user_id = up.user_id
-                                                                 JOIN tbl_project_roles pr ON pr.role_id = up.project_role_id`
-        },
-        create: {
-            insertProject: `INSERT INTO tbl_projects(name, participants_amount, tags_amount, description, date_start,
-                                               date_end)
-                      VALUES (?, ?, ?, ?, ?, ?)`,
-            selectNewProjectId: `SELECT LAST_INSERT_ID() as newProjectId`,
-            getInsertUsersSql: (projectId: number, participantIds: TProjectParticipantId[]) =>
-                `INSERT INTO tbl_users_of_projects (project_id, user_id, project_role_id)
-         VALUES ${participantIds
-             .map((p) => `(${projectId},${p.userId},${p.projectRoleId})`)
-             .toString()}`,
-            getInsertTagsSql: (
-                projectId: number,
-                tagIds: number[]
-            ) => `INSERT INTO tbl_tags_of_projects (project_id, tag_id)
-            VALUES ${tagIds.map((tagId) => '(' + projectId + ',' + tagId + ')').toString()}`
-        },
-        update: {
-            getUpdateProjectsSql: (
-                projectData: Pick<
-                    TProjectEdit,
-                    'projectId' | 'dateEnd' | 'dateStart' | 'description' | 'name'
-                >
-            ) => {
-                const { projectId, dateEnd, dateStart, name, description } = projectData;
-                return `UPDATE tbl_projects
-                SET ${concat([
-                    dateEnd ? "date_end = '" + dateEnd.trim() + "'" : '',
-                    dateStart ? "date_start = '" + dateStart.trim() + "'" : '',
-                    description !== undefined ? "description = '" + description.trim() + "'" : '',
-                    name ? "name = '" + name.trim() + "'" : ''
-                ])}
-                WHERE project_id = ${projectId}`;
-            },
-            getUpdateParticipantsSql: (
-                projectId: number,
-                participantIds: TProjectParticipantId[] | undefined,
-                deleteParticipantIds: number[] | undefined
-            ): TGetSqlReturn => {
-                const updateAmount = `UPDATE tbl_projects AS p
-                              SET p.participants_amount = (SELECT COUNT(up.project_id)
-                                                           FROM tbl_users_of_projects AS up
-                                                           WHERE up.project_id = ${projectId})
-                              WHERE p.project_id = ${projectId}`;
-                const insertSql =
-                    participantIds && this._sql.create.getInsertUsersSql(projectId, participantIds);
-                const deleteSql =
-                    deleteParticipantIds &&
-                    `DELETE
-           FROM tbl_users_of_projects
-           WHERE project_id = ${projectId}
-             AND user_id IN (${deleteParticipantIds.toString()}`;
-
-                return [insertSql, deleteSql, updateAmount];
-            },
-            getUpdateTagsSql: (
-                projectId: number,
-                tagIds: number[] | undefined,
-                deleteTagIds: number[] | undefined
-            ): TGetSqlReturn => {
-                const insertSql = tagIds && this._sql.create.getInsertTagsSql(projectId, tagIds);
-                const updateAmount = `UPDATE tbl_projects AS p
-                              SET p.tags_amount = (SELECT COUNT(tp.project_id)
-                                                   FROM tbl_tags_of_projects AS tp
-                                                   WHERE tp.project_id = ${projectId})
-                              WHERE p.project_id = ${projectId}`;
-                const deleteSql =
-                    deleteTagIds &&
-                    `DELETE
-           FROM tbl_tags_of_projects
-           WHERE project_id = ${projectId}
-             AND tag_id IN (${deleteTagIds.toString()})`;
-
-                return [insertSql, deleteSql, updateAmount];
-            }
-        }
-    };
-
     public deleteProject = async (projectId: number): Promise<TResponse> => {
         const { selectProjectName, deleteProject, deleteProjectUsers, deleteProjectTags } =
-            this._sql.delete;
+            deleteSql;
 
         const dbProjectNameResponse = await sqlPool.query(selectProjectName, projectId);
         const [[dbProjectName]] = dbProjectNameResponse;
@@ -171,7 +47,7 @@ class ProjectsServiceImpl implements ProjectsService {
     };
 
     public getProjects = async (...projectIds: number[]): Promise<TPayloadResponse<TProject[]>> => {
-        const { getSelectProjects, getSelectParticipants, getSelectTags } = this._sql.read;
+        const { getSelectProjects, getSelectParticipants, getSelectTags } = readSql;
 
         const dbProjectResponse = await sqlPool.query(getSelectProjects(projectIds));
         const [dbProjects] = dbProjectResponse as any[];
@@ -183,7 +59,9 @@ class ProjectsServiceImpl implements ProjectsService {
         const [dbParticipants] = dbParticipantsResponse as any[];
         if (dbParticipants.length)
             projects.forEach((project) => {
-                const participants = dbParticipants as Array<TDependency<TProjectParticipant>>;
+                const participants = dbParticipants as Array<
+                    TDependency<TProjectId, TProjectParticipant>
+                >;
                 project.participants = participants
                     .filter((participant) => participant.projectId === project.projectId)
                     .map((participant) => {
@@ -196,7 +74,7 @@ class ProjectsServiceImpl implements ProjectsService {
         const [dbTags] = dbTagsResponse as any[];
         if (dbTags.length)
             projects.forEach((project) => {
-                const tags = dbTags as Array<TDependency<TProjectTag>>;
+                const tags = dbTags as Array<TDependency<TProjectId, TProjectTag>>;
                 project.tags = tags
                     .filter((tag) => tag.projectId === project.projectId)
                     .map((tag) => {
@@ -214,7 +92,7 @@ class ProjectsServiceImpl implements ProjectsService {
     public createProject = async (projectData: TProjectCreation): Promise<TResponse> => {
         const { name, dateStart, dateEnd, participantIds, tagIds, description } = projectData;
         const { insertProject, selectNewProjectId, getInsertUsersSql, getInsertTagsSql } =
-            this._sql.create;
+            createSql;
 
         const connection = await sqlPool.getConnection();
         try {
@@ -295,8 +173,7 @@ class ProjectsServiceImpl implements ProjectsService {
             deleteTagIds,
             ...projectCommonData
         } = projectData;
-        const { getUpdateProjectsSql, getUpdateParticipantsSql, getUpdateTagsSql } =
-            this._sql.update;
+        const { getUpdateProjectsSql, getUpdateParticipantsSql, getUpdateTagsSql } = updateSql;
 
         const connection = await sqlPool.getConnection();
         try {
