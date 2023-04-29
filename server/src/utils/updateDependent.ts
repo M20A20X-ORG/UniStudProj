5a6fb9704108a034c07394dbb93c595a476674d3
Maksym Shapovalov <maks0681912507@gmail.com>
2023-04-30 01:38:37 +0300

    Fix(serv-dev): fix type of updateDependent util

--- server/src/utils/updateDependent.ts
+++ server/src/utils/updateDependent.ts
@@ -1,7 +1,24 @@
 import { QueryError } from 'mysql2';
 import { PoolConnection } from 'mysql2/promise';
 import { TGetSql } from '@type/sql';
+
 import { DataModificationError } from '@exceptions/DataModificationError';
+
+const handleError = (
+    error: unknown,
+    errNoRefStr: string,
+    errDupEntryStr: string = errNoRefStr
+): void => {
+    const { code } = error as QueryError;
+    let message = '';
+
+    if (code === 'ER_NO_REFERENCED_ROW_2') message = `Specified ${errNoRefStr} are not exists!`;
+    else if (code === 'ER_DUP_ENTRY')
+        message = `Specified ${errDupEntryStr} already added to this project!`;
+
+    if (message) throw new DataModificationError(message);
+    throw error;
+};
 
 export const updateDependent = async <T>(
     connection: PoolConnection,
@@ -11,21 +28,26 @@ export const updateDependent = async <T>(
     deleteData: number[] | undefined,
     errNoRefStr: string,
     errDupEntryStr: string = errNoRefStr
-) => {
+): Promise<void> => {
     try {
         const [insertSql, deleteSql, updateAmountSql] = getSql(id, data, deleteData);
         if (deleteSql) await connection.query(deleteSql);
-        if (insertSql) await connection.query(insertSql);
+        if (insertSql?.length) {
+            const promisesUpdate: Promise<void>[] = insertSql.map(
+                (sql) =>
+                    new Promise(async (resolve, reject) => {
+                        try {
+                            if (sql) await connection.query(sql);
+                            resolve();
+                        } catch (error: unknown) {
+                            reject(error);
+                        }
+                    })
+            );
+            await Promise.all(promisesUpdate);
+        }
         if ((deleteSql || insertSql) && updateAmountSql) await connection.query(updateAmountSql);
     } catch (error: unknown) {
-        const { code } = error as QueryError;
-        let message = '';
-
-        if (code === 'ER_NO_REFERENCED_ROW_2') message = `Specified ${errNoRefStr} are not exists!`;
-        else if (code === 'ER_DUP_ENTRY')
-            message = `Specified ${errDupEntryStr} already added to this project!`;
-
-        if (message) throw new DataModificationError(message);
-        throw error;
+        handleError(error, errNoRefStr, errDupEntryStr);
     }
 };
